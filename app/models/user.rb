53857eff8948b85b3965bcaed12c3d8fa12e6843
Charles Determan <cdetermanjr@hotmail.com>
2014-04-04 23:03:44 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	# before saving, convert email to lowercase
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
 	
 	# validate email format
@@ -12,4 +13,19 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates :password, length: { minimum: 6 }
+
+	# remember tokens
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+	def create_remember_token
+		self.remember_token = User.hash(User.new_remember_token)
+	end
 end
