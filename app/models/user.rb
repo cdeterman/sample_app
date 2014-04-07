e4d4e31d799b20c322ba7f4fa22891e85671d5c8
Charles Determan <cdetermanjr@hotmail.com>
2014-04-07 13:18:46 -0500

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id",
+		class_name: "Relationship",
+		dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
 
 	# before saving, convert email to lowercase
 	before_save { self.email = email.downcase }
@@ -29,6 +35,22 @@ class User < ActiveRecord::Base
 	# good practice as improves security of queries
 	def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+		relationships.create!(followed_id: other_user.id)
+	end
+
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def feed
+		Micropost.from_users_followed_by(self)
 	end
 
 	private
